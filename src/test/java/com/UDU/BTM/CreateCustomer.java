fc46ad70e7065bee95623b65c4d9e266f64ea9c7
jyotish12 <kjyotish03@gmail.com>
2023-01-28 11:58:33 +0530

    COMMIT1

---
+++ src/test/java/com/UDU/BTM/CreateCustomer.java
@@ -0,0 +1,13 @@
+package com.UDU.BTM;
+
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	
+	@Test
+	public void createTest1()
+	{
+		System.out.println("create success");
+	}
+
+}
